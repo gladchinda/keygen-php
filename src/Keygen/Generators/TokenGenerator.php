a1e98beff4ee51cde8601eb2279232886160d5ca
Glad Chinda <gladxeqs@gmail.com>
2018-07-15 23:59:37 +0100

    Modified generated tokens to exclude "=" sign

--- src/Keygen/Generators/TokenGenerator.php
+++ src/Keygen/Generators/TokenGenerator.php
@@ -17,14 +17,14 @@ class TokenGenerator extends Generator
 {
 	/**
 	 * Generates a random key.
-	 * 
+	 *
 	 * @param numeric $length
 	 * @return string
 	 */
 	protected function keygen($length)
 	{
 		$token = '';
-		$tokenlength = round($length * 3 / 4);
+		$tokenlength = round($length * 4 / 3);
 
 		for ($i = 0; $i < $tokenlength; ++$i) {
 			$token .= chr(rand(32,1024));
@@ -32,6 +32,6 @@ class TokenGenerator extends Generator
 
 		$token = base64_encode(str_shuffle($token));
 
-		return substr($token, -$length);
+		return substr($token, 0, $length);
 	}
 }
